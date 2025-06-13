4ff9d7f1b35604f4172491a4aff6eac2ff53ae3c
Curtis Loucks <clouc057@mtroyal.ca>
2025-06-13 12:49:47 -0600

    Initial commit for Skillsync backend and iOS app

---
+++ src/main/java/com/skillsync/backend/SkillsyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skillsync.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsyncBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillsync/backend/SkillsyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillsync.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
